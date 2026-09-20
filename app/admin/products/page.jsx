7f9faa1c7af35dba838ffcc5cd2502d11affc929
ibenfarikh-lab <ibenfarikh@gmail.com>
2026-09-20 12:28:50 +0700

    Add files via upload

--- app/admin/products/page.jsx
+++ app/admin/products/page.jsx
@@ -1,1 +1,9 @@
+'use client';
 
+import AdminProductPanel from '../../../components/AdminProductPanel';
+import { useAdminSession } from '../../../components/AdminSessionContext';
+
+export default function AdminProductsPage() {
+  const { tokoId, user } = useAdminSession();
+  return <AdminProductPanel tokoId={tokoId} authUser={user} />;
+}
