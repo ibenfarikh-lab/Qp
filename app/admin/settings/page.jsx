200bbda21166161c452c227a9b9e22cfb2518997
ibenfarikh-lab <ibenfarikh@gmail.com>
2026-09-20 12:29:30 +0700

    Add files via upload

--- app/admin/settings/page.jsx
+++ app/admin/settings/page.jsx
@@ -1,1 +1,9 @@
+'use client';
 
+import AdminStoreSettingsPanel from '../../../components/AdminStoreSettingsPanel';
+import { useAdminSession } from '../../../components/AdminSessionContext';
+
+export default function AdminSettingsPage() {
+  const { tokoId, user } = useAdminSession();
+  return <AdminStoreSettingsPanel tokoId={tokoId} authUser={user} />;
+}
