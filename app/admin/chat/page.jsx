6d26c6d8015f99d86f7effed0005515130ad574d
ibenfarikh-lab <ibenfarikh@gmail.com>
2026-09-20 12:26:46 +0700

    Add files via upload

--- app/admin/chat/page.jsx
+++ app/admin/chat/page.jsx
@@ -1,1 +1,9 @@
+'use client';
 
+import AdminChatPanel from '../../../components/AdminChatPanel';
+import { useAdminSession } from '../../../components/AdminSessionContext';
+
+export default function AdminChatPage() {
+  const { tokoId, user } = useAdminSession();
+  return <AdminChatPanel tokoId={tokoId} authUser={user} />;
+}
